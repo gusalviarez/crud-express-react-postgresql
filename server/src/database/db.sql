f451fec0ed8e753da207da06a046d6913bedabfe
gusalviarez <gusalv2001@gmail.com>
2024-05-22 14:34:43 -0400

    init db

---
+++ server/src/database/db.sql
@@ -0,0 +1,19 @@
+-- CREATE DATABASE firstapi;
+
+-- \l
+
+-- \c firstapi;
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(40),
+    created_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+INSERT INTO users (name, email)
+    VALUES ('joe'),
+    ('ryan');
+
+select * from users;
